771ecfcea793a22d29e42fd5110c48b27ac1a7bb
Shashikiran1297 <shashikiran1297@gmail.com>
2023-09-07 07:51:00 +0530

    added sucessfuly completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import NavBar from './Components/NavBar';
+import {Route,Routes} from "react-router-dom"
+import Home from "./Components/Home"
+import AddProduct from "./Components/AddProduct"
 
 function App() {
   return (
     <div className="App">
      <NavBar/>
+
+     <Routes>
+       <Route path='/' element={<Home/>} />
+       <Route path='/' element={<AddProduct/>} />
+     </Routes>
     </div>
   );
 }
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
   return (
@@ -11,10 +12,10 @@ const NavBar = () => {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <Link to="/" className="nav-link active" aria-current="page" href="#">Home</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Add product</a>
+          <Link to="/addProduct" className="nav-link active" aria-current="page" href="#">Add product</Link>
         </li>
       </ul>
     </div>
